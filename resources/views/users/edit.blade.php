db19cad708ab65f2f3b1d0e0f42f12afbbacb760
Yuriy2056757 <2056757@talnet.nl>
2019-09-12 12:17:30 +0000

    Add edit & show views

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,9 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            {{ Auth::user()->name }}
-
-            <a href="{{ route('users.edit', Auth::user()) }}">Edit</a>
+            EDIT
         </div>
     </div>
 </div>
