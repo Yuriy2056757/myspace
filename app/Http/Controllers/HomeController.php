36bb32abb9e68b3daaf777917d9d3c83ac61ffbe
yuriy-git <24823455+yuriy-git@users.noreply.github.com>
2019-09-20 06:24:39 +0200

    Show users on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,6 +14,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        // Retrieve all users
+        $users = User::all();
+
+        return view('home', compact('users'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,51 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="input-group mb-3">
+            <h1 class="text-center">Welcome to Myspace</h1>
+
+            <div class="input-group mb-3 mt-5">
                 <input type="text" class="form-control" placeholder="Search users...">
 
                 <div class="input-group-append">
                     <span class="input-group-text">Search</span>
                 </div>
             </div>
+
+            <div class="row mt-4">
+                @foreach ($users as $user)
+                    <div class="col-sm">
+                        <div class="card p-2 mb-3">
+                            <div>
+                                @if ($user->image)
+                                    <img
+                                        id="image_preview"
+                                        width="128"
+                                        height="128"
+                                        src="{{ asset('storage/' . Auth::user()->image) }}"
+                                        class="mb-3 rounded"
+                                    />
+                                @else
+                                    <img
+                                        id="image_preview"
+                                        width="128"
+                                        height="128"
+                                        src="{{ asset('placeholder-avatar.jpg') }}"
+                                        class="mb-3 rounded"
+                                    />
+                                @endif
+
+
+                                <div>
+                                    <div class="mt-2">
+                                        <a href="{{ route('users.show', $user) }}" class="btn btn-primary">
+                                            {{ Auth::check() ? $user->name . ' ' . $user->surname : $user->username }}'s Profile
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
         </div>
     </div>
 </div>
